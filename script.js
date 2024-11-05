b743681ce94b2ff1c60c0921e8042a729856bf91
Matheus lima <matheuslimadasilva0@gmail.com>
2024-11-05 20:40:29 -0300

    2040

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-
 //Aqui adicionamos um evento de escutar o click do mouse sobre o íconede hamburguee.
 hamburgerButton.addEventlistener("click", function () {
     mobillemenu.classList.add('flex');//Criamos uma função que a ouvir o click do mouse , ele adicione uma class, que está fazendo nossa caixa (div) aparecer, ou seja, ficar visível.
@@ -9,4 +6,22 @@ hamburgerButton.addEventlistener("click", function () {
 
 //criamos uma função que a ouvir o click do mouse, ele REMOVE uma class, que está fazendo nossa caixa (Div) desaparecer, ou seja, ficar inmvisível.
 closeButton.addEventlistener("click", function(){mobillemenu.classList.remove("flex");
+});
+
+window.reveal = ScrollReveal({reset:true});
+
+revelar.reveal('.efeito-img-topo',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+});
+
+revelar.reveal('.clientes-efeito',{
+    duration: 2000,
+    distance: "100px",
+});
+
+revelar.reveal('.principais-produtos',{
+    duration: 2000,
+    distance: '200px',
 });
