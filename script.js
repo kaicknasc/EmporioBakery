f09e691ee5d8b7167f81aac544de81f918673a20
Matheus lima <matheuslimadasilva0@gmail.com>
2024-11-05 21:14:36 -0300

    2114

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-//Aqui adicionamos um evento de escutar o click do mouse sobre o íconede hamburguee.
-hamburgerButton.addEventlistener("click", function () {
-    mobillemenu.classList.add('flex');//Criamos uma função que a ouvir o click do mouse , ele adicione uma class, que está fazendo nossa caixa (div) aparecer, ou seja, ficar visível.
-});
+window.revelar = ScrollReveal({reset:true});
 
 
-//criamos uma função que a ouvir o click do mouse, ele REMOVE uma class, que está fazendo nossa caixa (Div) desaparecer, ou seja, ficar inmvisível.
-closeButton.addEventlistener("click", function(){mobillemenu.classList.remove("flex");
-});
-
-window.reveal = ScrollReveal({reset:true});
-
-revelar.reveal('.efeito-img-topo',{
+revelar.reveal('.efeito-img-topo', {
     duration: 2000,
-    distance: '90px',
-    origin: 'left',
+    distance: '90px'
+})
+
+
+revelar.reveal('.clientes-efeito', {
+    duration: 2000,
+    distance: '120px'
+})
+
+revelar.reveal('.principais-produtos', {
+    duration: 2000,
+    distance: '200px'
+})
+
+
+
+
+//Aqui adicionamos um evento de escutar o click do Mouse sobre o ícone de Hamburguer.
+hamburgerButton.addEventListener("click", function() {
+    mobileMenu.classList.add("flex");
+    //Criamos uma função que a ouvir o click do Mouse, ele adicione uma Class, que está fazendo nossa caixa (Div) aparecer, ou seja, ficar visível.
 });
 
-revelar.reveal('.clientes-efeito',{
-    duration: 2000,
-    distance: "100px",
-});
-
-revelar.reveal('.principais-produtos',{
-    duration: 2000,
-    distance: '200px',
+//Criamos uma função que a ouvir o click do Mouse, ele REMOVE uma Class, que está fazendo nossa caixa (Div) desaparecer, ou seja, ficar invisível.
+closeButton.addEventListener("click", function() {
+    mobileMenu.classList.remove("flex");
 });
